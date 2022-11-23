21a5a3fe74123e4f9ccd90d2d04c42813bd5176b
Marijaradovanoviic <Marija.Radovanovic@swisscom.com>
2022-11-23 14:41:03 +0100

    rename the table Benutzer in benutzer

--- src/main/java/ch/zli/m223/model/Benutzer.java
+++ src/main/java/ch/zli/m223/model/Benutzer.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
@@ -20,6 +21,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 */
 
 @Entity
+@Table(name = "benutzer")
 public class Benutzer {
 
   @Id
