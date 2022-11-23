2a1b7b68632316cda86db250518fe8e39b1db38b
Marijaradovanoviic <Marija.Radovanovic@swisscom.com>
2022-11-23 14:42:26 +0100

    changed the import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,41 +1,32 @@
-INSERT INTO "Cafeteria"(type, preis)
-	VALUES ('spaghetti', 10);
-INSERT INTO "Cafeteria"(type, preis)
-	VALUES ('Nudeln', 23);
-INSERT INTO "Getränk"(type, preis)
-	VALUES ('Wasser', 5);
+INSERT INTO cafeteria (id, preis, type) VALUES (1, 2.4,'spaghetti');
+INSERT INTO cafeteria (id, preis, type) VALUES (2, 3.4,'Falafel');
 
-INSERT INTO "Rolle"(name)
-	VALUES ('admin');
-INSERT INTO "Rolle"(name)
-	VALUES ('mitglied');
 
-INSERT INTO "Benutzer"(email, nachname, vorname, password, rolle_id)
-	VALUES ('petermueller@gmail.com', 'Peter', 'Mueller', 'Mueller123', 1);
-INSERT INTO "Benutzer"(email, nachname, password, vorname, rolle_id)
-	VALUES ('mariamueller@gmail.com','maria', 'Mueller', 'maria123', 2);
-INSERT INTO "Benutzer"(email, nachname, password, vorname, rolle_id)
-	VALUES ('besucher@besucher.com','besucher', 'besucher', 'besucher', 2);
+INSERT INTO rolle(id, titel) VALUES (1, 'admin');
+INSERT INTO rolle(id, titel) VALUES (2, 'mitglied');
+
+INSERT INTO benutzer (rolle_id, cafeteria_id, vorname, password, nachname, email, id)
+	VALUES (1, 1, 'Peter', 'Mueller123', 'Mueller','petermueller@gmail.com', 1);
+
+INSERT INTO benutzer (rolle_id, cafeteria_id, vorname, password, nachname, email, id)
+	VALUES (2, 2, 'Abdullah', 'Abdu123', 'Abdullah','abduii@gmail.com', 2);
 
 INSERT INTO cafeteria_benutzer(
 	cafeteria_id, benutzer_id)
 	VALUES (1, 1);
 INSERT INTO cafeteria_benutzer(
 	cafeteria_id, benutzer_id)
-	VALUES (2, 1);
-INSERT INTO cafeteria_benutzer(
-	cafeteria_id, benutzer_id)
-	VALUES (3, 1);
+	VALUES (2, 2);
 
-INSERT INTO "Raum"(category, "ReservierungsStatus", name)
-    VALUES ('MeetingRaum', FALSE, 'MeetingRaum');
-INSERT INTO "Raum"(category, "ReservierungsStatus", name)
-    VALUES ('MeetingRaum2', TRUE, 'MeetingRaum2');
+INSERT INTO raum(reservierungstatus, name)
+    VALUES (FALSE, 'MeetingRaum');
+INSERT INTO raum(reservierungstatus, name)
+    VALUES (TRUE, 'MeetingRaum2');
 
-INSERT INTO "Buchung"(date, "halfday", "status", "stornierung", raum_id, benutzer_id)
-    VALUES ('2022-30-07', TRUE, FALSE, TRUE, 1, 1);
-INSERT INTO "Buchung"(date, "halfday", "status", "stornierung", raum_id, benutzer_id)
+INSERT INTO buchung(date, halfday, status, stornierung, raum_id, benutzer_id)
+    VALUES ('2022-07-07', TRUE, FALSE, TRUE, 1, 1);
+INSERT INTO buchung(date, halfday, status, stornierung, raum_id, benutzer_id)
     VALUES ('2022-05-05', FALSE, TRUE, TRUE, 2, 2);
-INSERT INTO "Buchung"(date, "halfday", "status", "stornierung", raum_id, benutzer_id)
-    VALUES ('2022-06-07', FALSE, FALSE,TRUE,  1, 3);
+INSERT INTO buchung(date, halfday, status, stornierung, raum_id, benutzer_id)
+    VALUES ('2022-06-07', FALSE, FALSE,TRUE,  2, 1);
     
